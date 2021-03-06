4f39750bedb7c92a731bf04a3992d52fdf6d9cf4
Julien Leblanc <julien.leblanc@epfedu.fr>
2021-03-06 18:44:27 +0100

    lab done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,39 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
+
   }
   startClick(callback) {
     // ... your code goes here
+    this.intervalId = setInterval(()=>{
+      this.currentTime ++
+      if (callback){callback()};
+    },1000);
   }
   getMinutes() {
     // ... your code goes here
+    return Number(Math.trunc(this.currentTime/60));
   }
   getSeconds() {
     // ... your code goes here
+    return Number(this.currentTime % 60);
   }
   twoDigitsNumber() {
     // ... your code goes here
+    return this.currentTime.toString().length === 2 ? this.currentTime.toString() : "0" + this.currentTime.toString();
   }
   stopClick() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
   resetClick() {
     // ... your code goes here
+    this.currentTime = 0;
   }
   splitClick() {
     // ... your code goes here
+    return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(this.getSeconds())}`;
   }
 }
