72938a2efab7660f6cf83df4d94a0e692d8a59e3
Ting-Chi Wong <tcwong007@gmail.com>
2021-03-07 03:24:21 +0100

    class Chronometer & startClick() done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,25 +1,33 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
+
   startClick(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(currentTime => this.currentTime++, 1000);
   }
+
   getMinutes() {
     // ... your code goes here
   }
+
   getSeconds() {
     // ... your code goes here
   }
+
   twoDigitsNumber() {
     // ... your code goes here
   }
+
   stopClick() {
     // ... your code goes here
   }
+
   resetClick() {
     // ... your code goes here
   }
+  
   splitClick() {
     // ... your code goes here
   }
