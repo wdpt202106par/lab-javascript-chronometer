873278b2cb8779e1e713966ed9c60533ef8de4b2
Julien Leblanc <julien.leblanc@epfedu.fr>
2021-03-09 21:16:15 +0100

    iteration 2 almost done

--- javascript/index.js
+++ javascript/index.js
@@ -15,14 +15,21 @@ let splits = document.getElementById('splits');
 
 function printTime() {
   // ... your code goes here
+  printMinutes();
+  printSeconds();
 }
 
 function printMinutes() {
   // ... your code goes here
+  minUni.innerHTML = chronometer.splitClick()[1];
+  minDec.innerHTML = chronometer.splitClick()[0];
+  
 }
 
 function printSeconds() {
-  // ... your code goes here
+  // ... your code goes here  
+  secUni.innerHTML = chronometer.splitClick()[4];
+  secDec.innerHTML = chronometer.splitClick()[3];  
 }
 
 // ==> BONUS
@@ -57,9 +64,27 @@ function setResetBtn() {
 // Start/Stop Button
 btnLeft.addEventListener('click', () => {
   // ... your code goes here
+  let btnLeftStatus = btnLeft.innerHTML;
+
+  if (btnLeftStatus === "START"){
+    btnLeft.setAttribute('class','btn stop');
+    btnLeft.innerHTML = "STOP";
+    btnRight.setAttribute('class','btn split');
+    btnRight.innerHTML = "SPLIT";
+    chronometer.startClick(printTime);
+  }
+  if (btnLeftStatus === "STOP"){
+    btnLeft.setAttribute('class','btn start');
+    btnLeft.innerHTML = "START";
+    btnRight.setAttribute('class','btn reset');
+    btnRight.innerHTML = "RESET";  
+    chronometer.stopClick();  
+  }
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', () => {
   // ... your code goes here
+  chronometer.resetClick();
+  printTime();
 });
